6904fbc96010fb78da80433eea579ef1bd426dbe
Administrator <Administrator@DrASHFAQ-PC>
2021-03-23 12:57:53 +0530

    Ver1

---
+++ MyArtim1/src/pack1/MyCalculator1.java
@@ -0,0 +1,22 @@
+package pack1;
+
+public class MyCalculator1 {
+	double a,b,c;
+	public double add(double a,double b)
+	{
+		c=a+b;
+		return c;
+	}
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyCalculator1 m=new MyCalculator1();
+		double add=m.add(12.0,56.45);
+		System.out.println("Add="+add);
+		
+		
+
+	}
+
+}
