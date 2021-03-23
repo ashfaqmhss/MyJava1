0d084c5719f627fa0e100167978a4314a6f2570d
Administrator <Administrator@DrASHFAQ-PC>
2021-03-23 13:03:45 +0530

    Ver2

--- MyArtim1/src/pack1/MyCalculator1.java
+++ MyArtim1/src/pack1/MyCalculator1.java
@@ -7,6 +7,11 @@ public class MyCalculator1 {
 		c=a+b;
 		return c;
 	}
+	public double sub(double a,double b)
+	{
+		c=a-b;
+		return c;
+	}
 	
 
 	public static void main(String[] args) {
@@ -14,7 +19,8 @@ public class MyCalculator1 {
 		MyCalculator1 m=new MyCalculator1();
 		double add=m.add(12.0,56.45);
 		System.out.println("Add="+add);
-		
+		double sub=m.sub(12.0,14.7);
+		System.out.println("Sub="+sub);
 		
 
 	}
